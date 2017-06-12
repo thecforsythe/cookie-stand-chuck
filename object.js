27ba3a464399b639bfda2b3228e3a9b306755e3f
Chuck Forsythe <thecforsythe@gmail.com>
2017-06-12 14:06:07 -0500

    sales projections entered

---
+++ object.js
@@ -0,0 +1,24 @@
+var firstAndPike = {
+  minNumberCustomers: 23,
+  maxNumberCustomers: 65,
+  aveCookieSale: 6.3,
+};
+var seaTacAirport = {
+  minNumberCustomers: 3,
+  maxNumberCustomers: 24,
+  aveCookieSale: 1.2,
+};
+var seatleCenter = {
+  minNumberCustomers: 11,
+  maxNumberCustomers: 38,
+  aveCookieSale: 3.7,
+};
+var capitolHill = {
+  minNumberCustomers: 20,
+  maxNumberCustomers: 38,
+  aveCookieSale: 2.3,
+};var Alki = {
+  minNumberCustomers: 2,
+  maxNumberCustomers: 16,
+  aveCookieSale: 4.6,
+};
