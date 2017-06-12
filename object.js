d8e0f308337a7e52f926661f4aad3f09b6522a0a
Chuck Forsythe <thecforsythe@gmail.com>
2017-06-12 17:03:16 -0500

    next step

--- object.js
+++ object.js
@@ -1,8 +1,26 @@
 var firstAndPike = {
+  hourOpen: 6,
+  hourClosed: 20,
   minNumberCustomers: 23,
   maxNumberCustomers: 65,
   aveCookieSale: 6.3,
+  randomCustomerPerHour: function() {
+    return Math.floor(Math.random() * (this.maxNumberCustomers - this.minNumberCustomers + 1)) + this.minNumberCustomers;
+  },
+  simulateCookiesPerHour: function() {
+    var hours = [];
+    for(var i = 0; i < this.hourClosed - this.hourOpen; i++) {
+      var customerCount = this.randomCustomerPerHour();
+      var time = (i + 6);
+      console.log('hours: ' + time + ', cookies: ' + customerCount);
+      hours[i] = customerCount * this.aveCookieSale;
+      console.log(hours);
+      this.cookiesPerHour = hours;
+    }
+  },
 };
+firstAndPike.simulateCookiesPerHour();
+
 var seaTacAirport = {
   minNumberCustomers: 3,
   maxNumberCustomers: 24,
